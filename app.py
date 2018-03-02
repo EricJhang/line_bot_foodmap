d4b6ea72ba746fd848f75e2841b2a7a46f6b7906
artopll <artopllting@gmail.com>
2018-03-02 17:30:57 +0800

    add image info

--- app.py
+++ app.py
@@ -56,15 +56,24 @@ def handle_lcationmessage(event):
     req = requests.get(url)#發送請求
     foodinfo = json.loads(req.text)
     for i in range(len(foodinfo['results'])):
-        print(foodinfo['results'][i]["name"])    
+        photo_reference = foodinfo['results'][i]["photos"]["photo_reference"]
+        url_photo = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=100&photoreference="+photo_reference+"&key="+googlekey
+        req_photo = requests.get(url_photo)
+        print(type(req_photo))    
+        message_photo = ImageSendMessage(
+            original_content_url=req_photo,
+            preview_image_url=req_photo
+        )
+        push_message(event.source.user_id,message_photo)
         message = LocationSendMessage(
             title=foodinfo['results'][i]["name"],
             address=foodinfo['results'][i]["vicinity"],
             latitude=foodinfo['results'][i]["geometry"]["location"]["lat"],
             longitude=foodinfo['results'][i]["geometry"]["location"]["lng"]
-        ) 
+        )
         #message = str(foodinfo['results'][i]["name"])+"分數:"+str(foodinfo['results'][i]["rating"])+"\n"+message
-        push_message(event.source.user_id,message)    
+        push_message(event.source.user_id,message)
+        if(i >=5): break    
     print(req.text)
     
 @handler.add(MessageEvent, message=TextMessage)
