852ac13ee28357c138d8a9608808dfdf6f7d6119
artopll <artopllting@gmail.com>
2018-03-02 18:12:03 +0800

    change tpye

--- app.py
+++ app.py
@@ -62,8 +62,8 @@ def handle_lcationmessage(event):
         print(type(req_photo))
         print(req_photo.headers['content-type'])    
         message_photo = ImageSendMessage(
-            original_content_url=req_photo.raw,
-            preview_image_url=req_photo.raw
+            original_content_url=req_photo.files,
+            preview_image_url=req_photo.files
         )
         push_message(event.source.user_id,message_photo)
         message = LocationSendMessage(
