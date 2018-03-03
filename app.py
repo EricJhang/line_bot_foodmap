3e5de90353e54b0f125cfbed78b25fc32deb412e
artopll <artopllting@gmail.com>
2018-03-03 15:45:28 +0800

    fixed bug

--- app.py
+++ app.py
@@ -92,14 +92,14 @@ def handle_lcationmessage(event):
                         label='位置',
                         uri=address_url
                     )]
-            columns_list.append(CarouselColumn(thumbnail_image_url = url_photo,title = foodinfo['results'][i]['name'] ,text="網友推薦指數:"+str(foodinfo['results'][i]['rating']),actions=actions_tmp))          
+            columns_list.append(CarouselColumn(thumbnail_image_url = url_photo,title = foodinfo['results'][i]['name'] ,text="網友推薦指數:"+str(foodinfo['results'][i]['rating']),actions=actions_tmp))
         if(i >=10): 
             i = len(foodinfo['results'])
             break            
     carousel_template_message = TemplateSendMessage(
         alt_text='Carousel template',
         template=CarouselTemplate(
-                columns=columns_list                        
+                columns=columns_list
         )
     )
     push_message(push_userid,carousel_template_message)
