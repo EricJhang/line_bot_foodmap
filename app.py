c358a214e398ee287d9bd04352ce2b0914863cea
artopll <artopllting@gmail.com>
2018-03-03 15:46:47 +0800

    fiex bud 02

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ def handle_lcationmessage(event):
                         label='位置',
                         uri=address_url
                     )]
-            columns_list.append(CarouselColumn(thumbnail_image_url = url_photo,title = foodinfo['results'][i]['name'] ,text="網友推薦指數:"+str(foodinfo['results'][i]['rating']),actions=actions_tmp))
+            columns_list.append(CarouselColumn(thumbnail_image_url = url_photo,title = foodinfo['results'][i]['name'],text="網友推薦指數:"+str(foodinfo['results'][i]['rating']),actions=actions_tmp))
         if(i >=10): 
             i = len(foodinfo['results'])
             break            
@@ -102,6 +102,7 @@ def handle_lcationmessage(event):
                 columns=columns_list
         )
     )
+    print(carousel_template_message)
     push_message(push_userid,carousel_template_message)
     #print(req.text)    
 @handler.add(MessageEvent, message=TextMessage)
