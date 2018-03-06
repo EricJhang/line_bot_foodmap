7f2ff2e3bb7c0f79a896261ded0d9fa54fc8f98f
artopll <artopllting@gmail.com>
2018-03-06 22:45:24 +0800

    fixed

--- app.py
+++ app.py
@@ -149,15 +149,20 @@ def handle_message(event):
                     req_detal = requests.get(url_detal)#發送請求
                     detal_json = json.loads(req_detal.text)
                     reviews_text_01 =""
-                    reviews_text_02 =""                     
+                    reviews_text_02 =""                    
                     if("reviews" in detal_json['result']):
                         if(len(detal_json['result']["reviews"])>=2):
-                            reviews_text_01 ="評分:"+detal_json['result']["reviews"][0]["rating"]+"/5\n"+"評論:"+detal_json['result']["reviews"][0]["text"]+"\n"
-                            reviews_text_02 ="評分:"+detal_json['result']["reviews"][1]["rating"]+"/5\n"+"評論:"+detal_json['result']["reviews"][1]["text"]+"\n"
+                            
+                            #reviews_text_01 ="評分:"+detal_json['result']["reviews"][0]["rating"]+"/5\n"+"評論:"+detal_json['result']["reviews"][0]["text"]+"\n"
+                            #reviews_text_02 ="評分:"+detal_json['result']["reviews"][1]["rating"]+"/5\n"+"評論:"+detal_json['result']["reviews"][1]["text"]+"\n"
+                            print(detal_json['result']["reviews"])
+                            print(len(detal_json['result']["reviews"]))
                         elif(len(detal_json['result']["reviews"])==1):
-                            reviews_text_01 ="評分:"+detal_json['result']["reviews"][0]["rating"]+"/5\n"+"評論:"+detal_json['result']["reviews"][0]["text"]+"\n"
+                            #reviews_text_01 ="評分:"+detal_json['result']["reviews"][0]["rating"]+"/5\n"+"評論:"+detal_json['result']["reviews"][0]["text"]+"\n"
+                            reviews_text_02 ="沒有評論"
                         else:
                             reviews_text_01 ="沒有評論"
+                            reviews_text_02 ="沒有評論" 
                     if(url_photo != "" ):                        
                         columns_list.append(
                         CarouselColumn(
@@ -170,15 +175,7 @@ def handle_message(event):
                                     text=label_string
                                 ),
                                 URITemplateAction(label='位置',uri=address_url)]
-                                ),
-                                MessageTemplateAction(
-                                    label="評論1",
-                                    text=reviews_text_01
-                                ),
-                                MessageTemplateAction(
-                                    label="評論2",
-                                    text=reviews_text_02
-                                )
+                                )                                
                         )
                     #push_message(event.source.user_id,carousel_template_message)
                 if(i >=9): 
